f5f77f6bca64d842048a41403063035c2b1cad48
ShaileshMaddikera <maddikera7191@mydusd.org>
2025-03-29 16:04:05 -0700

    1.0.2

--- Student.java
+++ Student.java
@@ -1,13 +1,41 @@
+// Student.java
 /*
  * Activity 4.9.7
- */ 
-public class Student 
-{ 
+ */
+public class Student
+{
   private String name;
+  private int id;
+  private int year;
 
-  Student(String name) 
-  { 
+  Student(String name, int id, int year)
+  {
     this.name = name;
-  } 
+    this.id = id;
+    this.year = year;
+  }
 
-} 
+  /**
+   *  toString method
+   *  @return a string representation of the object
+   */
+  public String toString()
+  {
+    return "Name: " + name + ", ID: " + id + ", Year: " + year;
+  }
+
+  /**
+   *  equals method
+   *  @param other the object to compare to
+   *  @return true if the objects are equal, false otherwise
+   */
+  public boolean equals(Object other)
+  {
+    if (other == null || !(other instanceof Student))
+    {
+      return false;
+    }
+    Student otherStudent = (Student) other;
+    return this.id == otherStudent.id;
+  }
+}
--- StudentRunner.java
+++ StudentRunner.java
@@ -1,16 +1,24 @@
+// StudentRunner.java
 /*
  * Activity 4.9.7
- */ 
+ */
 public class StudentRunner
-{ 
-  public static void main(String args[]) 
-  { 
-    Student s1 = new Student("Alice"); 
-    Student s2 = new Student("Alice");
-    Student s3 = new Student("Bob");
+{
+  public static void main(String args[])
+  {
+    Student s1 = new Student("Alice", 1234, 9);
+    Student s2 = new Student("Alice", 1234, 9);
+    Student s3 = new Student("Bob", 5678, 10);
+    Student s4 = new Student("Charlie", 9012, 11);
 
-    // Below two statements are equivalent 
-    System.out.println(s1); 
-    System.out.println(s1.toString()); 
-  } 
-} 
+    // Below two statements are equivalent, now will print the overridden toString
+    System.out.println(s1);
+    System.out.println(s1.toString());
+
+    System.out.println("\nTesting equals method:");
+    System.out.println("s1.equals(s2): " + s1.equals(s2)); // Should be true (same ID)
+    System.out.println("s1.equals(s3): " + s1.equals(s3)); // Should be false (different ID)
+    System.out.println("s1.equals(s4): " + s1.equals(s4)); // Should be false (different ID)
+    System.out.println("s3.equals(s4): " + s3.equals(s4)); // Should be false (different ID)
+  }
+}
